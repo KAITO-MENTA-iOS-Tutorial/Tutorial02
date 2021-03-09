3bfd1e4ebbf88eb983d135e408f46a7e1416b72b
Kaito Hattori <kaichan394@gmail.com>
2021-03-09 13:06:11 +0900

    first commit

--- Tutorial02/ViewController.swift
+++ Tutorial02/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    var num: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.numberLabel.text = String(self.num)
     }
-
-
+    
+    @IBAction func onClickCountUp(_ sender: Any) {
+        self.num = self.num + 1
+        self.numberLabel.text = String(self.num)
+    }
+    
 }
-
